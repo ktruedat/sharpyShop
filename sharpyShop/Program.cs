c4aafae90d626827ce277973f6d1dc384eae8275
ktruedat <crinnic77@gmail.com>
2024-02-07 23:42:03 +0200

    first commit

--- sharpyShop/Program.cs
+++ sharpyShop/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-
+// builder.Services.AddScoped<>()
 var app = builder.Build();
 
 app.UseStaticFiles();
