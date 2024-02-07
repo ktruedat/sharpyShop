8ffd8c70d342f04baf95f5fea37a5871fa6451ae
ktruedat <crinnic77@gmail.com>
2024-02-08 00:05:07 +0200

    added category and IProductRepository interface

--- sharpyShop/Models/Product.cs
+++ sharpyShop/Models/Product.cs
@@ -5,7 +5,7 @@ public class Product
     private int ProductId { get; set; }
     private String Name { get; set; } = string.Empty;
     private int CategoryId { get; set; }
-    private Category Category { get; set; }
+    private Category Category { get; set; }  = default!;
     private decimal Price { get; set; }
     private int ProducerId { get; set; }
     private Producer Producer { get; set; } = default!;
