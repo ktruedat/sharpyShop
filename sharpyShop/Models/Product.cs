2234f0d81646500bbfbf4c0c2aabf99b7171eae9
ktruedat <crinnic77@gmail.com>
2024-02-08 00:00:01 +0200

    created product model

---
+++ sharpyShop/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace sharpyShop.Models;
+
+public class Product
+{
+    private int ProductId { get; set; }
+    private String Name { get; set; } = string.Empty;
+    private int CategoryId { get; set; }
+    private Category Category { get; set; }
+    private decimal Price { get; set; }
+    private int ProducerId { get; set; }
+    private Producer Producer { get; set; } = default!;
+}
