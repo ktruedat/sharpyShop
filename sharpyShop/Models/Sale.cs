45cd80341ef6635f3f9895cd0c2c828b63bbd51b
ktruedat <crinnic77@gmail.com>
2024-02-10 22:27:34 +0200

    Addes Sale model

--- sharpyShop/Models/Sale.cs
+++ sharpyShop/Models/Sale.cs
@@ -2,5 +2,11 @@ namespace sharpyShop.Models;
 
 public class Sale
 {
-    
+    private int Id { get; set; }
+    private int ProductId { get; set; }
+    private int CustomerId { get; set; }
+    private decimal Quantity { get; set; }
+    private int Amount { get; set; }
+    private int PromotionId { get; set; }
+    private DateTime Date { get; set; }
 }
