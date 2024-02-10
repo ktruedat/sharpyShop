7aca6e75611d049f014d67b4aad6cc4df2720f6e
ktruedat <crinnic77@gmail.com>
2024-02-10 22:40:15 +0200

    added customer model

---
+++ sharpyShop/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace sharpyShop.Models;
+
+public class Customer
+{
+    private int Id { get; set; }
+    private String Name { get; set; }
+    private String Email { get; set; }
+    private String Phone { get; set; }
+}
